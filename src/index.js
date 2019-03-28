d4e55b819842f5ca4a5490473c66719134903672
TAKENET\pauloh <pauloh@take.net>
2019-03-28 18:00:38 -0300

    get apis and api key

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ var app = require('./services/appservice');
 require('./controllers/root');
 
 app.init();
+
+
+//APIKey Google
+//AIzaSyDIPN1WOrn8WeATWKKmFTlQLuulgqX3jO4
+
+
+// pegar id do local
+//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=<NOME DO LOCAL>&inputtype=textquery&fields=place_id,name,rating,opening_hours,geometry&key=AIzaSyDIPN1WOrn8WeATWKKmFTlQLuulgqX3jO4
+
+
+//procurar lugares perto
+//https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.497912,19.040235&radius=20000&type=restaurant&key=AIzaSyDIPN1WOrn8WeATWKKmFTlQLuulgqX3jO4
+//https://developers.google.com/places/web-service/supported_types
+
+
