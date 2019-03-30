9963277f9dbfa415f67edf643f6d0fbbd65d6553
ceifa <gabrinelson27@gmail.com>
2019-03-30 12:19:01 -0300

    Initial project configuration

--- TG.Api/Startup.cs
+++ TG.Api/Startup.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Lime.Protocol;
+using Lime.Protocol.Serialization.Newtonsoft;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace TG.Api
 {
     public class Startup
     {
+        private const string HealthEndpoint = "/health";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,7 +22,13 @@ namespace TG.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services
+                .AddHealthChecks();
+
+            services
+                .AddMvc()
+                .AddJsonOptions(o => JsonNetSerializer.Settings.Converters.ForEach(c => o.SerializerSettings.Converters.Add(c)))
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,11 +41,13 @@ namespace TG.Api
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app
+                    .UseHsts()
+                    .UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseHealthChecks(HealthEndpoint)
+                .UseMvc();
         }
     }
 }
